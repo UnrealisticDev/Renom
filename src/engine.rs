68d10eadeb5e79f2674e2ad53d8b360e8efa8e35
msmoiz <mustafa.moiz125@gmail.com>
2022-06-01 23:52:44 +0000

    Add basic error handling to engine.
    Simply prints out the error and aborts further processing.

--- src/engine.rs
+++ src/engine.rs
@@ -1,52 +1,78 @@
 use ini::Ini;
 use regex::Regex;
 
-use crate::change::Change;
+use crate::{
+    change::{AppendIniEntry, Change, RenameFile, ReplaceInFile, SetIniEntry},
+    logger::Log,
+};
 
 pub fn execute(changeset: Vec<Change>) {
     for change in changeset {
-        match change {
-            Change::RenameFile(params) => {
-                println!("Renaming {:?} to {:?}", &params.from, &params.to);
-                std::fs::rename(params.from, params.to).unwrap()
-            }
-            Change::ReplaceInFile(params) => {
-                println!(
-                    "Replacing {} with {} in {:?}",
-                    &params.from, &params.to, &params.path
-                );
-                let data = std::fs::read_to_string(&params.path).unwrap();
-                let regex = Regex::new(&params.from).unwrap();
-                let data_after_replace = regex.replace_all(&data, params.to.as_str()).to_string();
-                std::fs::write(&params.path, &data_after_replace).unwrap();
-            }
-            Change::SetIniEntry(params) => {
-                println!(
-                    "Setting ini entry [{}] {} = {} in {:?}",
-                    &params.section, &params.key, &params.value, &params.path
-                );
-                let mut config = Ini::load_from_file(&params.path).unwrap();
-                config
-                    .with_section(Some(params.section))
-                    .set(params.key, params.value);
-                config.write_to_file(params.path).unwrap();
-            }
-            Change::AppendIniEntry(params) => {
-                println!(
-                    "Appending ini entry [{}] {} = {} in {:?}",
-                    &params.section, &params.key, &params.value, &params.path
-                );
-                let mut config = Ini::load_from_file(&params.path).unwrap();
-                config
-                    .with_section(Some(&params.section))
-                    .set("dummy", "dummy");
-                config
-                    .section_mut(Some(&params.section))
-                    .unwrap()
-                    .append(&params.key, &params.value);
-                config.with_section(Some(&params.section)).delete(&"dummy");
-                config.write_to_file(&params.path).unwrap();
+        let result = match change {
+            Change::RenameFile(params) => rename_file(params),
+            Change::ReplaceInFile(params) => replace_in_file(params),
+            Change::SetIniEntry(params) => set_ini_entry(params),
+            Change::AppendIniEntry(params) => append_ini_entry(params),
+        };
+
+        match result {
+            Ok(_) => {}
+            Err(err) => {
+                Log::error(err.to_string());
+                break;
             }
         }
     }
+}
+
+fn rename_file(params: RenameFile) -> std::io::Result<()> {
+    let message = format!("Renaming {:?} to {:?}", &params.from, &params.to);
+    Log::step("Apply", &message);
+    std::fs::rename(params.from, params.to)?;
+    Ok(())
+}
+
+fn replace_in_file(params: ReplaceInFile) -> std::io::Result<()> {
+    let message = format!(
+        "Replacing {} with {} in {:?}",
+        &params.from, &params.to, &params.path
+    );
+    Log::step("Apply", &message);
+    let data = std::fs::read_to_string(&params.path)?;
+    let regex = Regex::new(&params.from).unwrap(); // @todo: How do we want to handle this error?
+    let data_after_replace = regex.replace_all(&data, params.to.as_str()).to_string();
+    std::fs::write(&params.path, &data_after_replace)?;
+    Ok(())
+}
+
+fn set_ini_entry(params: SetIniEntry) -> std::io::Result<()> {
+    let message = format!(
+        "Setting ini entry [{}] {} = {} in {:?}",
+        &params.section, &params.key, &params.value, &params.path
+    );
+    Log::step("Apply", &message);
+    let mut config = Ini::load_from_file(&params.path).unwrap(); // @todo: Coerce to io result?
+    config
+        .with_section(Some(params.section))
+        .set(params.key, params.value);
+    config.write_to_file(params.path)?;
+    Ok(())
+}
+
+fn append_ini_entry(params: AppendIniEntry) -> std::io::Result<()> {
+    println!(
+        "Appending ini entry [{}] {} = {} in {:?}",
+        &params.section, &params.key, &params.value, &params.path
+    );
+    let mut config = Ini::load_from_file(&params.path).unwrap(); // @todo: Coerce to io result?
+    config
+        .with_section(Some(&params.section))
+        .set("dummy", "dummy");
+    config
+        .section_mut(Some(&params.section))
+        .unwrap()
+        .append(&params.key, &params.value);
+    config.with_section(Some(&params.section)).delete(&"dummy");
+    config.write_to_file(&params.path)?;
+    Ok(())
 }
