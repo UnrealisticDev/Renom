0a150c7db1f0bb84412b27c670cf3f196c1eb64f
msmoiz <mustafa.moiz125@gmail.com>
2022-06-02 03:10:22 +0000

    Implement backup mechanism.
    Before any file is changed, its content is backed up into a backup
    folder (`.renom/backup` in the typical case). The backed up file is
    named using the SHA256 hash of the content, similar to the Git style.
    This allows us to avoid naming conflicts/overwriting a backup file when
    the filename is the same but content differs.

--- src/engine.rs
+++ src/engine.rs
@@ -1,37 +1,40 @@
+use std::path::{Path, PathBuf};
+
 use ini::Ini;
 use regex::Regex;
+use sha2::{Digest, Sha256};
 
 use crate::{
     changes::{AppendIniEntry, Change, RenameFile, ReplaceInFile, SetIniEntry},
     logger::Log,
 };
 
-pub fn execute(changeset: Vec<Change>) {
-    for change in changeset {
+// @todo: Introduce a revert mechanism
+// @todo: Keep track of progress
+pub fn execute(changeset: Vec<Change>, backup_dir: impl AsRef<Path>) {
+    for change in &changeset {
         Log::process(format!("{}", change));
         let result = match change {
             Change::RenameFile(params) => rename_file(params),
-            Change::ReplaceInFile(params) => replace_in_file(params),
-            Change::SetIniEntry(params) => set_ini_entry(params),
-            Change::AppendIniEntry(params) => append_ini_entry(params),
+            Change::ReplaceInFile(params) => replace_in_file(params, backup_dir.as_ref()),
+            Change::SetIniEntry(params) => set_ini_entry(params, backup_dir.as_ref()),
+            Change::AppendIniEntry(params) => append_ini_entry(params, backup_dir.as_ref()),
         };
 
-        match result {
-            Ok(_) => {}
-            Err(err) => {
-                Log::error(err.to_string());
-                break;
-            }
+        if let Err(err) = result {
+            Log::error(err.to_string());
+            break;
         }
     }
 }
 
-fn rename_file(params: RenameFile) -> std::io::Result<()> {
-    std::fs::rename(params.from, params.to)?;
+fn rename_file(params: &RenameFile) -> std::io::Result<()> {
+    std::fs::rename(&params.from, &params.to)?;
     Ok(())
 }
 
-fn replace_in_file(params: ReplaceInFile) -> std::io::Result<()> {
+fn replace_in_file(params: &ReplaceInFile, backup_dir: &Path) -> std::io::Result<()> {
+    backup_file(&params.path, backup_dir)?;
     let data = std::fs::read_to_string(&params.path)?;
     let regex = Regex::new(&params.from).unwrap(); // @todo: How do we want to handle this error?
     let data_after_replace = regex.replace_all(&data, params.to.as_str()).to_string();
@@ -39,16 +42,18 @@ fn replace_in_file(params: ReplaceInFile) -> std::io::Result<()> {
     Ok(())
 }
 
-fn set_ini_entry(params: SetIniEntry) -> std::io::Result<()> {
+fn set_ini_entry(params: &SetIniEntry, backup_dir: &Path) -> std::io::Result<()> {
+    backup_file(&params.path, backup_dir)?;
     let mut config = Ini::load_from_file(&params.path).unwrap(); // @todo: Coerce to io result?
     config
-        .with_section(Some(params.section))
-        .set(params.key, params.value);
-    config.write_to_file(params.path)?;
+        .with_section(Some(&params.section))
+        .set(&params.key, &params.value);
+    config.write_to_file(&params.path)?;
     Ok(())
 }
 
-fn append_ini_entry(params: AppendIniEntry) -> std::io::Result<()> {
+fn append_ini_entry(params: &AppendIniEntry, backup_dir: &Path) -> std::io::Result<()> {
+    backup_file(&params.path, backup_dir)?;
     let mut config = Ini::load_from_file(&params.path).unwrap(); // @todo: Coerce to io result?
     config
         .with_section(Some(&params.section))
@@ -60,4 +65,14 @@ fn append_ini_entry(params: AppendIniEntry) -> std::io::Result<()> {
     config.with_section(Some(&params.section)).delete(&"dummy");
     config.write_to_file(&params.path)?;
     Ok(())
+}
+
+fn backup_file(file: &Path, backup_dir: &Path) -> std::io::Result<PathBuf> {
+    let content = std::fs::read_to_string(file)?;
+    let mut hasher = Sha256::new();
+    hasher.update(&content);
+    let hash = hasher.finalize();
+    let backup_file = backup_dir.join(format!("{:x}", hash));
+    std::fs::write(&backup_file, &content)?;
+    Ok(backup_file)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
-mod pathfinder;
+mod changes;
+mod changesets;
+mod engine;
 mod logger;
+mod pathfinder;
 mod renamer;
 
 use logger::Log;
 use renamer::Renamer;
 
-use std::io::{stdin, Read};
+use std::{
+    io::{stdin, Read},
+    path::PathBuf,
+};
 
 fn main() {
+    let root = PathBuf::from("test/TestEngineVersion4");
+    let changeset = changesets::generate_code_changeset(
+        "TestEngineVersion4",
+        "TestEngineVersion4Post",
+        "test/TestEngineVersion4",
+        vec![PathBuf::from(
+            "Source/TestEngineVersion4/TestEngineVersion4GameModeBase.h",
+        )],
+        vec![],
+    );
+    std::fs::create_dir_all(root.join(".renom/backup")).unwrap();
+    engine::execute(changeset, root.join(".renom/backup"));
+    return;
+
     let mut renamer = Renamer::new();
     if let Err(e) = renamer.start() {
         Log::error(format!("Project rename failed: {}", e));
