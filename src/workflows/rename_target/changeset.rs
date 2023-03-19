38666f36eba74bc9423aae5f80a43139affc328b
msmoiz <mustafa.moiz125@gmail.com>
2023-03-19 14:16:06 -0700

    feat: rename cross-target references
    It is possible for a project to have multiple build targets and for one
    target to depend on another target. It is not clear how common this is,
    but this is used in the Lyra starter project. When a project target is
    renamed, references to that target must be updated in other targets as
    well.

--- src/workflows/rename_target/changeset.rs
+++ src/workflows/rename_target/changeset.rs
@@ -10,6 +10,7 @@ use super::context::Context;
 /// Generate a changeset to rename an Unreal Engine target.
 pub fn generate_changeset(context: &Context) -> Vec<Change> {
     let Context {
+        project_targets,
         target_target: Target {
             name: old_name,
             path: target_file,
@@ -18,12 +19,43 @@ pub fn generate_changeset(context: &Context) -> Vec<Change> {
         ..
     } = context;
 
-    // @todo: rename references in other targets
+    let mut changeset = vec![];
 
-    vec![
-        rename_target_class(target_file, old_name, new_name),
-        rename_target_file(target_file, new_name),
-    ]
+    changeset.push(rename_target_class(target_file, old_name, new_name));
+    changeset.push(rename_target_file(target_file, new_name));
+    changeset.extend(rename_cross_target_references(
+        target_file,
+        project_targets,
+        old_name,
+        new_name,
+    ));
+
+    changeset
+}
+
+fn rename_cross_target_references(
+    target_file: &Path,
+    project_targets: &[Target],
+    old_name: &str,
+    new_name: &str,
+) -> Vec<Change> {
+    project_targets
+        .iter()
+        .filter(|target| &&target.path != &target_file)
+        .map(|target| rename_target_references_in_target(&target.path, old_name, new_name))
+        .collect()
+}
+
+fn rename_target_references_in_target(
+    target_file: &Path,
+    old_name: &str,
+    new_name: &str,
+) -> Change {
+    Change::ReplaceInFile(ReplaceInFile::new(
+        target_file,
+        format!("{}Target", old_name),
+        format!("{}Target", new_name),
+    ))
 }
 
 fn rename_target_class(target_file: &Path, old_name: &str, new_name: &str) -> Change {
